c6163189facd4e4daf82a73bc34087488e8e3c26
한기 <@DESKTOP-JEUU9FA>
2024-04-08 22:12:44 +0900

    test

---
+++ src/main/java/com/mgds/project/MgdsServerApplication.java
@@ -0,0 +1,13 @@
+package com.mgds.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MgdsServerApplication
+{
+	public static void main(String[] args)
+	{
+		SpringApplication.run(MgdsServerApplication.class, args);
+	}
+}
---
+++ src/test/java/com/mgds/project/MgdsServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mgds.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MgdsServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
