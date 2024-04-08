952c802eba819a4d9ff906e88bd9428061163196
한기 <@DESKTOP-JEUU9FA>
2024-04-08 23:29:43 +0900

    이거 올려놔

---
+++ src/main/java/com/mgds/project/MgdsServerApplication3.java
@@ -0,0 +1,13 @@
+package com.mgds.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MgdsServerApplication3
+{
+	public static void main(String[] args)
+	{
+		SpringApplication.run(MgdsServerApplication3.class, args);
+	}
+}
